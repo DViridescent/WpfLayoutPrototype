272551b236ee3a9788e41b1baedcbc7cd0e6e506
呆维iridescent <475430703@qq.com>
2024-04-02 14:34:00 +0800

    添加项目文件。

---
+++ MyControls/MyButtonWithText.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyControls
+{
+    /// <summary>
+    /// MyButtonWithText.xaml 的交互逻辑
+    /// </summary>
+    public partial class MyButtonWithText : UserControl
+    {
+        public static readonly DependencyProperty ButtonCommandProperty = DependencyProperty.Register(
+            "ButtonCommand", typeof(ICommand), typeof(MyButtonWithText), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty ButtonTextProperty = DependencyProperty.Register(
+            "ButtonText", typeof(string), typeof(MyButtonWithText), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty DescriptionTextProperty = DependencyProperty.Register(
+            "DescriptionText", typeof(string), typeof(MyButtonWithText), new PropertyMetadata(null));
+
+
+        public MyButtonWithText()
+        {
+            InitializeComponent();
+        }
+
+        public ICommand ButtonCommand
+        {
+            get { return (ICommand)GetValue(ButtonCommandProperty); }
+            set { SetValue(ButtonCommandProperty, value); }
+        }
+
+        public string ButtonText
+        {
+            get { return (string)GetValue(ButtonTextProperty); }
+            set { SetValue(ButtonTextProperty, value); }
+        }
+
+        public string DescriptionText
+        {
+            get { return (string)GetValue(DescriptionTextProperty); }
+            set { SetValue(DescriptionTextProperty, value); }
+        }
+    }
+}
---
+++ MyLayouts/StandardLayout.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyLayouts
+{
+    /// <summary>
+    /// StandardLayout.xaml 的交互逻辑
+    /// </summary>
+    public partial class StandardLayout : UserControl
+    {
+        public static readonly DependencyProperty ToolsAreaProperty = DependencyProperty.Register(
+            "ToolsArea", typeof(object), typeof(StandardLayout), new PropertyMetadata(null));
+        public static readonly DependencyProperty ContentAreaProperty = DependencyProperty.Register(
+            "ContentArea", typeof(object), typeof(StandardLayout), new PropertyMetadata(null));
+
+        public StandardLayout()
+        {
+            InitializeComponent();
+        }
+
+        public object ToolsArea
+        {
+            get { return GetValue(ToolsAreaProperty); }
+            set { SetValue(ToolsAreaProperty, value); }
+        }
+        public object ContentArea
+        {
+            get { return GetValue(ContentAreaProperty); }
+            set { SetValue(ContentAreaProperty, value); }
+        }
+    }
+}
